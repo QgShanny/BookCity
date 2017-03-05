dddf955ed28e7500e577a4b4b51cce75287e57a2
ShannyShanny <869209311@qq.com>
2017-03-05 14:23:04 +0800

    阅读器 设置 v1

--- js/reader.js
+++ js/reader.js
@@ -1,22 +1,21 @@
 var flagBar = 0;
 var flagFont = 0; //字体背景的设置标识
 $("#bookcontainer").on("tap", function() {
-	if(flagFont == 1){
-		$("#detailSet").css("bottom","-172px");
+	if(flagFont == 1) {
+		$("#detailSet").css("bottom", "-172px");
 		flagFont = 0;
-	}
-	else{
-	if(flagBar == 0) {
-		$("#readerHead").css("top", "0px");
-		$("#readerSet").css("bottom", "0px");
-		flagBar = 1;
 	} else {
-		$("#readerHead").css("top", "-44px");
-		$("#readerSet").css("bottom", "-80px");
-		flagBar = 0;
-	}
-	$("#readerHead").css("transition", "0.5s");
-	$("#readerSet").css("transition", "0.5s");
+		if(flagBar == 0) {
+			$("#readerHead").css("top", "0px");
+			$("#readerSet").css("bottom", "0px");
+			flagBar = 1;
+		} else {
+			$("#readerHead").css("top", "-44px");
+			$("#readerSet").css("bottom", "-80px");
+			flagBar = 0;
+		}
+		$("#readerHead").css("transition", "0.5s");
+		$("#readerSet").css("transition", "0.5s");
 	}
 });
 
@@ -33,12 +32,45 @@ $("#readerSet li:nth-of-type(2) a").on('tap', function() {
 	}
 })
 
-
-$("#readerSet ul li:nth-of-type(3)").on("tap",function(){
-	$("#readerHead").css("top","-44px");
-	$("#readerSet").css("bottom","-80px")
-	$("#detailSet").css("bottom","0");
+$("#readerSet ul li:nth-of-type(3)").on('tap', function() {
+	$("#readerHead").css("top", "-44px");
+	$("#readerSet").css("bottom", "-80px")
+	$("#detailSet").css("bottom", "0");
 	flagFont = 1;
 	flagBar = 0;
 });
 
+$("#jianFontSize").on('tap',function(){
+	var temp = $("#fontSizeSpan").text();
+	temp = parseInt(temp);
+	if(temp == 10){
+		return;
+	}
+	temp -= 1;
+	$("#fontSizeSpan").text(temp);
+	$("#bookcontainer h4").css("font-size",temp+2+"px");
+	$("#bookcontainer p").css("font-size",temp+"px");
+})
+$("#addFontSize").on('tap',function(){
+	var temp = $("#fontSizeSpan").text();
+	temp = parseInt(temp);
+	if(temp == 30){
+		return;
+	}
+	temp += 1;
+	$("#fontSizeSpan").text(temp);
+	$("#bookcontainer h4").css("font-size",temp+2+"px");
+	$("#bookcontainer p").css("font-size",temp+"px");
+})
+
+window.onload = function() {
+	var spanLen = document.getElementById("fontColorDiv").getElementsByTagName("span");
+	for(var i = 0; i < spanLen.length; i++) {
+		(function() {
+			var p = i + 1;
+			$("#fontColorDiv span:nth-of-type(" + (i + 1) + ")").on('tap', function() {
+				$("#bookcontainer").css("background", $("#fontColorDiv span:nth-of-type(" + p + ")>i").css("background"));
+			});
+		})();
+	}
+}

823d5333c7e48a8ad5ccbbce69cc0feaed661075
ShannyShanny <869209311@qq.com>
2017-03-26 23:55:01 +0800

    测试滑动

--- js/reader.js
+++ js/reader.js
@@ -8,8 +8,19 @@ require.config({
 	}
 
 });
-define(['jquery', 'mui'], function() {
+define(['jquery', 'mui'], function() {	
 
+	mui.init()
+	mui.ready(function() {
+		mui('.mui-scroll-wrapper').scroll({
+			deceleration: 0.0005
+		});
+	});
+
+	//返回首页
+	$("#backIndex").on('tap',function(){
+		window.location.href = '../index.html';
+	});
 	var flagBar = 0;
 	var flagFont = 0; //字体背景的设置标识
 	$("#bookcontainer").on("tap", function() {
@@ -19,12 +30,16 @@ define(['jquery', 'mui'], function() {
 			flagFont = 0;
 		} else {
 			if(flagBar == 0) {
-				$("#readerHead").css("top", "0px");
-				$("#readerSet").css("bottom", "0px");
+//				$("#readerHead").css("top", "0px");
+//				$("#readerSet").css("bottom", "0px");
+				$("#readerHead").fadeIn('fast');
+				$("#readerSet").fadeIn('fast');
 				flagBar = 1;
 			} else {
-				$("#readerHead").css("top", "-44px");
-				$("#readerSet").css("bottom", "-80px");
+//				$("#readerHead").css("top", "-44px");
+//				$("#readerSet").css("bottom", "-80px");
+				$("#readerHead").fadeOut('fast');
+				$("#readerSet").fadeOut('fast');
 				flagBar = 0;
 			}
 			$("#readerHead").css("transition", "0.5s");
@@ -94,45 +109,100 @@ define(['jquery', 'mui'], function() {
 	$("#moreColor").bind('DOMNodeInserted', function(e) {
 		alert('element now contains: ' + $(e.target).html());
 	});
-	
+
 	//双击回顶部
-	$("#readerHead").on('tap',function(){
+	$("#readerHead").on('tap', function() {
 		var time = new Date().getTime();
-		$("#readerHead").on('tap',function(){
-			if(new Date().getTime() - time < 1000){
-				$('html,body').stop().animate({scrollTop: '0px'},500)				
+		$("#readerHead").on('tap', function() {
+			if(new Date().getTime() - time < 1000) {
+				$('html,body').stop().animate({
+					scrollTop: '0px'
+				}, 500)
 			}
+			//window.scrollTo(0,0); 
 		});
 	})
 
 	var bookNode = 1;
 	getArticle(bookNode);
-	function getArticle(bookNode){
-		$.getJSON("../../json/test"+bookNode+".json", function(data) {
+
+	function getArticle(bookNode) {
+		$.getJSON("../../json/test" + bookNode + ".json", function(data) {
 			var $jsontip = $("#article");
 			var strHtml = ""; //存储数据的变量 
 			$jsontip.empty(); //清空内容 
 			$.each(data, function(infoIndex, info) {
 				var contentLen = info.content.length;
 				strHtml += "<h4 class='articleTitle'>" + info["title"] + "</h4>";
-				for(var i = 0;i<contentLen;i++){
+				for(var i = 0; i < contentLen; i++) {
 					strHtml += "<p>" + info["content"][i]["detail"] + "</p>";
 				}
-			})
-			$jsontip.html(strHtml); //显示处理后的数据 
+			});
+			strHtml += "<div id='articleBtn'><button type='button' class='mui-btn mui-btn-outlined' id='pre'>上一章</button><button type='button' class='mui-btn mui-btn-outlined' id='next'>下一章</button></div>";
+			$jsontip.html(strHtml); //显示处理后的数据
+			//			$("#articleBtn").before(strHtml);
 		})
 	}
-	$("#pre").on('tap',function(){
-//		$("#bookcontainer").unbind();
-		bookNode--;
-		getArticle(bookNode);
-		$('html,body').stop().animate({scrollTop: '0px'},0)
-	});	
-	$("#next").on('tap',function(){
-//		$("#bookcontainer").unbind();
-		bookNode++;
-		getArticle(bookNode);
-		$('html,body').stop().animate({scrollTop: '0px'},0)
+	$(function() {
+		$(document).on('tap', '#pre', function() {
+			console.log(1);
+			bookNode--;
+			getArticle(bookNode);
+			$('html,body').stop().animate({
+				scrollTop: '0px'
+			}, 0)
+		});
+		$(document).on('tap', '#next', function() {
+			bookNode++;
+			getArticle(bookNode);
+			$('html,body').stop().animate({
+				scrollTop: '0px'
+			}, 0)
+		});
+	})
+	
+	$("#readerSet ul li:first-of-type").on('tap',function(){
+		$("#catalog").css("left","0");
+		$("#readerHead,#readerSet").fadeOut('fast');
+		$("#catalog").css('transition','0.5s');
+		
 	});
-
+	$("#back").on('tap',function(){
+		$("#catalog").css('left','100%');
+		$("#catalog").css('transition','0.5s');
+	});
+	
+//	目录里的目录,书签,笔记
+		var spanLen = document.getElementById("fontColorDiv").getElementsByTagName("span");
+	for(var i = 0; i < spanLen.length; i++) {
+		(function() {
+			var p = i + 1;
+			$("#fontColorDiv span:nth-of-type(" + p + ")").on('tap', function() {
+				$("#bookcontainer").css("background", $("#fontColorDiv span:nth-of-type(" + p + ")>i").css("background"));
+			});
+		})();
+	}
+	
+	for(var i = 0;i<3;i++){
+		(function(){
+			var p = i+1;
+			$("#catalogClass li:nth-of-type("+p+")").on('tap',function(){
+				$("#catalogClass li:nth-of-type("+p+")").css("border-bottom","solid #23a3d5 2px");
+				$("#catalogClass li:nth-of-type("+p+")").siblings().css("border-bottom","none");
+				$(".catalogD").css("display","none");
+				$(".catalogD").eq(p-1).css("display","block");
+			});
+		})()
+	}
+//	目录里的章节
+	var chapterLen = $(".chapter li").length;
+	for(var i = 0;i<chapterLen;i++){
+		(function(){
+			var p = i+1;
+			$(".chapter li:nth-of-type("+p+") a").on('tap',function(){
+				getArticle(p);
+				$("#catalog").css("left","100%");
+			});
+		})()
+	}
 })
